47365c3e2480ed69ad42648566e475bfc805e38d
Zach Dubinsky <zdubinsk@travelers.com>
2023-07-12 13:25:10 -0400

    Home view improvements

--- client/src/components/HomeView.js
+++ client/src/components/HomeView.js
@@ -1,5 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./View.css";
+import logo from "./logo.png";
 
 export default function HomeView() {
-  return <div> Hello World</div>;
+  const [searchInput, setSearchInput] = useState("");
+
+  return (
+    <div className="wrapper">
+      <div className="top">
+        <img style={{ width: 150, height: 125 }} src={logo} />
+
+        <input type="text" placeholder="Search..." value={searchInput} />
+      </div>
+      <div className="left"> Left</div>
+      <div className="right"> Right</div>
+      <div className="center"> Cetner</div>
+    </div>
+  );
 }
