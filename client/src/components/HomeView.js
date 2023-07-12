a2a332054a41faf9a4477bb1c4e6112c6319f144
Zach Dubinsky <zdubinsk@travelers.com>
2023-07-12 19:35:38 -0400

    css grid -> react-grid library

--- client/src/components/HomeView.js
+++ client/src/components/HomeView.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-grid";
 import "./HomeView.css";
 import NA from "./NA.jpg";
 
@@ -6,43 +7,51 @@ export default function HomeView() {
   const [searchInput, setSearchInput] = useState("");
 
   return (
-    <div className="grid-container">
-      <div className="item1">
-        <img style={{ width: 150, height: 125 }} src={NA} />
-        Games
-      </div>
-      <div className="item2">
-        <img style={{ width: 150, height: 125 }} src={NA} />
-        Games
-      </div>
-      <div className="item3">
-        <img style={{ width: 150, height: 125 }} src={NA} />
-        Games
-      </div>
-      <div className="item4">
-        <img style={{ width: 150, height: 125 }} src={NA} />
-        Games
-      </div>
-      <div className="item5">
-        <img style={{ width: 150, height: 125 }} src={NA} />
-        Games
-      </div>
-      <div className="item6">
-        <img style={{ width: 150, height: 125 }} src={NA} />
-        Games
-      </div>
-      <div className="item7">
-        <img style={{ width: 150, height: 125 }} src={NA} />
-        Games
-      </div>
-      <div className="item8">
-        <img style={{ width: 150, height: 125 }} src={NA} />
-        Games
-      </div>
-      <div className="item9">
-        <img style={{ width: 150, height: 125 }} src={NA} />
-        Games
-      </div>
+    <div>
+      <Container>
+        <Row>
+          <Col>
+            <img style={{ width: 150, height: 125 }} src={NA} />
+            Games
+          </Col>
+          <Col>
+            <img style={{ width: 150, height: 125 }} src={NA} />
+            Games
+          </Col>
+          <Col>
+            <img style={{ width: 150, height: 125 }} src={NA} />
+            Games
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <img style={{ width: 150, height: 125 }} src={NA} />
+            Games
+          </Col>
+          <Col>
+            <img style={{ width: 150, height: 125 }} src={NA} />
+            Games
+          </Col>
+          <Col>
+            <img style={{ width: 150, height: 125 }} src={NA} />
+            Games
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <img style={{ width: 150, height: 125 }} src={NA} />
+            Games
+          </Col>
+          <Col>
+            <img style={{ width: 150, height: 125 }} src={NA} />
+            Games
+          </Col>
+          <Col>
+            <img style={{ width: 150, height: 125 }} src={NA} />
+            Games
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
