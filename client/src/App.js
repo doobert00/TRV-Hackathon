4b7946bd6f93d0a7e485e089ad7da8adb37f2164
Zach Dubinsky <zdubinsk@travelers.com>
2023-07-12 14:54:11 -0400

    App.js design done+ old app depricated

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,26 @@
 import React, { useState, useEffect } from "react";
 import { Route, Routes, Navigate, Link } from "react-router-dom";
-import "./View.css";
+import "./App.css";
 import logo from "./components/logo.png";
 
+const categoryLink =
+  "https://5190-2603-7080-2001-3b05-285b-82f8-6e37-c08d.ngrok-free.app/api/categories";
+
+async function fetchCategories() {
+  const res = await fetch(categoryLink);
+  return await res.json();
+}
+
 function App() {
-  fetch("http://localhost:3000/")
-    .then((res) => {
-      return res.json();
-    })
-    .then((data) => {
-      console.log(data);
-    });
   const [searchInput, setSearchInput] = useState("");
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetchCategories().then((cats) => {
+      setCategories(cats);
+    });
+  }, []);
+
   return (
     <div className="wrapper">
       <div className="top">
@@ -24,7 +33,13 @@ function App() {
           value={searchInput}
         />
       </div>
-      <div className="left"> Left</div>
+      <div className="left">
+        <ul>
+          {categories.map((cat, index) => {
+            return <li key={index}>{cat}</li>;
+          })}
+        </ul>
+      </div>
       <div className="right"> Right</div>
       <div className="center"> Cetner</div>
     </div>
--- client/src/components/HomeView.js
+++ client/src/components/HomeView.js
@@ -5,16 +5,5 @@ import logo from "./logo.png";
 export default function HomeView() {
   const [searchInput, setSearchInput] = useState("");
 
-  return (
-    <div className="wrapper">
-      <div className="top">
-        <img style={{ width: 150, height: 125 }} src={logo} />
-
-        <input type="text" placeholder="Search..." value={searchInput} />
-      </div>
-      <div className="left"> Left</div>
-      <div className="right"> Right</div>
-      <div className="center"> Cetner</div>
-    </div>
-  );
+  return <div className="wrapper"></div>;
 }
