b7186eae50a1f3bc7482e9a338e16ef1f9d1d3cb
Zach Dubinsky <zdubinsk@travelers.com>
2023-07-13 11:03:03 -0400

    More routing + UI updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,54 @@
 import React, { useState, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
+import { Container, Row, Col } from "react-grid";
 import "./App.css";
 
 import ItemView from "./components/ItemView";
 import CategoryView from "./components/CategoryView";
 import HomeView from "./components/HomeView.js";
+import CategoryPane from "./components/CategoryPane";
 
 import logo from "./components/logo.png";
-
 function App() {
   const [searchInput, setSearchInput] = useState("");
-  const [categories, setCategories] = useState([]);
-
-  /*
-  useEffect(() => {
-    fetchCategories().then((cats) => {
-      setCategories(cats);
-    });
-  }, []);
-  */
 
   return (
-    <div className="wrapper">
-      <div className="top">
-        <img style={{ width: 150, height: 125 }} src={logo} />
-
-        <input
-          type="text"
-          placeholder="Search..."
-          onChange={(event) => setSearchInput(event.target.value)}
-          value={searchInput}
-        />
-      </div>
-      <div className="left"></div>
-      <div className="right"> Right</div>
-      <div className="center">
-        <Routes>
-          <Route exact path="/" element={<HomeView />} />
-          <Route path="/item" element={<ItemView />} />
-        </Routes>
-      </div>
+    <div>
+      <Container>
+        <div className="top">
+          <Row>
+            <Col>
+              <img style={{ width: 150, height: 125 }} src={logo} />
+            </Col>
+            <Col>
+              <input
+                type="text"
+                placeholder="Search..."
+                onChange={(event) => setSearchInput(event.target.value)}
+                value={searchInput}
+              />
+            </Col>
+          </Row>
+        </div>
+        <Row>
+          <div className="left">
+            <Col>
+              <CategoryPane />
+            </Col>
+          </div>
+          <div className="center">
+            <Col>
+              <Routes>
+                <Route exact path="/" element={<HomeView />} />
+                <Route path="/item/:id" element={<ItemView />} />
+              </Routes>
+            </Col>
+          </div>
+          <Col>
+            <div className="right"> Right</div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
