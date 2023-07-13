48ff25579919c742e0aacf9a2f827af1215cec34
Zach Dubinsky <zdubinsk@travelers.com>
2023-07-13 11:15:40 -0400

    react routing pulls item from DB

--- client/src/components/ItemView.js
+++ client/src/components/ItemView.js
@@ -18,25 +18,19 @@ export default function ItemView() {
   useEffect(() => {
     getItem(`http://localhost:3000/api/products/${id}`).then((res) => {
       setItem(res);
-      console.log(res);
     });
   }, []);
 
-  console.log(item.name);
   return (
     <div className={"grid-container"}>
       <div className="item1">
         <img src=""></img>
       </div>
 
-      <div className="item2">
-        <h4>Reviews</h4>
-      </div>
-
       <div className="item3">
-        <h4>{item.id}</h4>
-        <p>Fetch Price</p>
-        <p>Fetch item description</p>
+        <h4>Name: {item.name}</h4>
+        <p> Price: {item.price}</p>
+        <p> Description: {item.description}</p>
         <input
           type={"button"}
           value="Add to Cart"
