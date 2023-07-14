98d93d21136705a5c82a2a46553f976f9ae192b3
Zach Dubinsky <zdubinsk@travelers.com>
2023-07-14 17:03:51 -0400

    checkout functionality, no post yet

--- client/src/components/CartView.js
+++ client/src/components/CartView.js
@@ -1,15 +1,32 @@
-import React, { useState, useEffect } from "react";
-import { Container } from "react-grid";
+import React, { useState, useEffect, useCallback } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+const postOrder = async (items, cost) => {
+  //POST the order
+  sessionStorage.clear();
+  sessionStorage.setItem("num_entries", 0);
+};
 
 export default function CartView() {
   const [items, setItems] = useState([]);
+  const [cost, setCost] = useState(0);
+
+  const nav = useNavigate();
+  const handleNav = useCallback(() => nav(`/`, { replace: true }), [nav]);
+  const handleCheckout = async () => {
+    postOrder(items, cost);
+    handleNav();
+  };
 
   useEffect(() => {
     let tmp = [];
+    let tmpCost = 0;
     for (let i = 0; i < parseInt(sessionStorage.getItem("num_entries")); i++) {
       tmp.push(JSON.parse(sessionStorage.getItem(i.toString())));
+      tmpCost += parseFloat(tmp[i].price.substring(1));
     }
     setItems([...tmp]);
+    setCost(tmpCost);
   }, []);
 
   return (
@@ -17,12 +34,16 @@ export default function CartView() {
       {items.map((item) => (
         <p>
           <Link to={`/item/${item.id}`}>
-            {item.name} : {item.price}
+            {item.name}: {item.price}
           </Link>
         </p>
       ))}
-      <p> Cost: </p>
-      <p> Buy Now Button</p>
+      <p> Cost: ${cost}</p>
+      <input
+        type={"button"}
+        value="Check Out"
+        onClick={() => handleCheckout()}
+      />
     </div>
   );
 }
