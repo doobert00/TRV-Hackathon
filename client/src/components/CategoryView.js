c1f666a47ff2e87d4f3e7c2d5029aec63ea37234
Zach Dubinsky <zdubinsk@travelers.com>
2023-07-14 11:42:25 -0400

    cat names display. BUG: Duplicate names are fetched

--- client/src/components/CategoryView.js
+++ client/src/components/CategoryView.js
@@ -12,142 +12,42 @@ const get = async (url) => {
   });
   return await res.json();
 };
+
 export default function CategoryView() {
   const { id } = useParams();
-  const [category, setCategory] = useState([]);
+  const [items, setItems] = useState([]);
   const [index, setIndex] = useState(0);
 
   useEffect(() => {
-    get(`http://localhost:3000/api/categories/${id}`).then((res) => {
-      setCategory(res);
-      console.log(res);
-    });
-  });
-  const [item1, setItem1] = useState([]);
-  const [item2, setItem2] = useState([]);
-  const [item3, setItem3] = useState([]);
-  const [item4, setItem4] = useState([]);
-  const [item5, setItem5] = useState([]);
-  const [item6, setItem6] = useState([]);
-  /*
-  useEffect(() => {
-    console.log(category);
-    get(`http://localhost:3000/api/products/${category.products[1]}`).then(
-      (res) => {
-        setItem1(res);
-      }
-    );
-  }, []); 
-  useEffect(() => {
-    get(
-      `http://localhost:3000/api/products/${category.products[index + 1]}`
-    ).then((res) => {
-      setItem2(res);
-    });
+    get(`http://localhost:3000/api/categories/${id}`)
+      .then((res) => {
+        return res;
+      })
+      .then((res) => {
+        let usedIndices = [];
+        setItems([]);
+        for (let i = 0; i < res.products.length; i++) {
+          if (!usedIndices.includes(i)) {
+            usedIndices.push(i);
+            let x = res.products[i];
+            get(`http://localhost:3000/api/products/${x}`).then((prod) => {
+              let tmp = items;
+              tmp.push(prod);
+              tmp = [...new Set(tmp)];
+              setItems([...tmp]);
+            });
+          }
+        }
+      });
   }, []);
-  useEffect(() => {
-    get(
-      `http://localhost:3000/api/products/${category.products[index + 2]}`
-    ).then((res) => {
-      setItem3(res);
-    });
-  }, []);
-  useEffect(() => {
-    get(
-      `http://localhost:3000/api/products/${category.products[index + 3]}`
-    ).then((res) => {
-      setItem4(res);
-    });
-  }, []);
-  useEffect(() => {
-    get(
-      `http://localhost:3000/api/products/${category.products[index + 4]}`
-    ).then((res) => {
-      setItem5(res);
-    });
-  }, []);
-  useEffect(() => {
-    get(
-      `http://localhost:3000/api/products/${category.products[index + 5]}`
-    ).then((res) => {
-      setItem6(res);
-    });
-  }, []);*/
+
   return (
     <div>
-      {" "}
-      {/*
-      <Container>
-        <Row>
-          <Col>
-            <Row>
-              <img style={{ width: 150, height: 125 }} src={NA} />
-            </Row>
-            <Row>
-              <Link to={`/item/${item1.id}`}>{item1.name}</Link>
-            </Row>
-            <Row> {item1.price}</Row>
-          </Col>
-          <Col>
-            <Row>
-              <img style={{ width: 150, height: 125 }} src={NA} />
-            </Row>
-            <Row>
-              <Link to={`/item/${item2.id}`}>{item2.name}</Link>
-            </Row>
-            <Row> {item2.price}</Row>
-          </Col>
-          <Col>
-            <Container>
-              <Row>
-                <img style={{ width: 150, height: 125 }} src={NA} />
-              </Row>
-              <Row>
-                <Link to={`/item/${item3.id}`}>{item3.name}</Link>
-              </Row>
-              <Row> {item3.price}</Row>
-            </Container>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Row>
-              <img style={{ width: 150, height: 125 }} src={NA} />
-            </Row>
-            <Row>
-              <Link to={`/item/${item4.id}`}>{item4.name}</Link>
-            </Row>
-            <Row> {item4.price}</Row>
-          </Col>
-          <Col>
-            <Row>
-              <img style={{ width: 150, height: 125 }} src={NA} />
-            </Row>
-            <Row>
-              <Link to={`/item/${item5.id}`}>{item5.name}</Link>
-            </Row>
-            <Row> {item5.price}</Row>
-          </Col>
-          <Col>
-            <Row>
-              <img style={{ width: 150, height: 125 }} src={NA} />
-            </Row>
-            <Row>
-              {" "}
-              <Link to={`/item/${item6.id}`}>{item6.name}</Link>
-            </Row>
-            <Row> {item6.price}</Row>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <div className="leftbtn"></div>
-          </Col>
-          <Col>
-            <div className="rightbtn"></div>
-          </Col>
-        </Row>
-      </Container>*/}
+      <Container style={{ display: "flex", flexWrap: "wrap" }}>
+        {items.map((item) => (
+          <p>{item.name}</p>
+        ))}
+      </Container>
     </div>
   );
 }
